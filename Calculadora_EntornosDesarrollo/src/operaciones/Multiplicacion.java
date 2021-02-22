7fe591e0e770c0e6cb4dd88e5b9a1efc53c8b9f7
ThattaPy <56270213+ThattaPY@users.noreply.github.com>
2021-02-22 21:06:34 +0100

    Eliminadas tildes en comentarios

--- Calculadora_EntornosDesarrollo/src/operaciones/Multiplicacion.java
+++ Calculadora_EntornosDesarrollo/src/operaciones/Multiplicacion.java
@@ -12,49 +12,49 @@ public class Multiplicacion {
 
 	
 	/**
-	 * Multiplicación de dos números reales
-	 * @param coefficientOne Numero real. Primer factor de la multiplicación
-	 * @param coefficientTwo Numero real. Segundo factor de la multiplicación
-	 * @return  Numero real. Resultado de la operación de multiplicación
+	 * Multiplicacion de dos numeros reales
+	 * @param coefficientOne Numero real. Primer factor de la multiplicacion
+	 * @param coefficientTwo Numero real. Segundo factor de la multiplicacion
+	 * @return  Numero real. Resultado de la operacion de multiplicacion
 	 */
 	public float twoRealNumbersProduct(float coefficientOne, float coefficientTwo) {
 		float result = coefficientOne * coefficientTwo;
 		return result;
-	} // Cierre del método
+	} // Cierre del mï¿½todo
 	
 	/**
-	 * Multiplicación de dos números enteros
-	 * @param coefficientOne Numero entero. Primer factor de la multiplicación
-	 * @param coefficientTwo Numero entero. Segundo factor de la multiplicación
-	 * @return  Numero entero. Resultado de la operación de multiplicación
+	 * Multiplicacion de dos numeros enteros
+	 * @param coefficientOne Numero entero. Primer factor de la multiplicacion
+	 * @param coefficientTwo Numero entero. Segundo factor de la multiplicacion
+	 * @return  Numero entero. Resultado de la operacion de multiplicacion
 	 */
 	public int twoIntegerNumersProduct(int coefficientOne, int coefficientTwo) {
 		int result = coefficientOne * coefficientTwo;
 		return result;
-	} // Cierre del método
+	} // Cierre del metodo
 	
 	/**
-	 * Multiplicación de tres números reales
-	 * @param coefficientOne Primer factor de la multiplicación
-	 * @param coefficientTwo Segundo factor de la multiplicación
-	 * @param coefficientThree Tercer factor de la multiplicación
-	 * @return Resultado de la operación de multiplicación
+	 * Multiplicacion de tres numeros reales
+	 * @param coefficientOne Primer factor de la multiplicacion
+	 * @param coefficientTwo Segundo factor de la multiplicacion
+	 * @param coefficientThree Tercer factor de la multiplicacion
+	 * @return Resultado de la operacion de multiplicacion
 	 */
 	public float threeRealNumbersProduct(float coefficientOne, float coefficientTwo, float coefficientThree) {
 		float result = coefficientOne * coefficientTwo * coefficientThree;
 		return result;
-	} // Cierre del método
+	} // Cierre del metodo
 	
 	/**
-	 * Potencia de dos números reales
+	 * Potencia de dos numeros reales
 	 * @param coefficientOne Base de la potencia
 	 * @param coefficientTwo Exponente de la potencia
-	 * @return Resultado de la operación base elevado a exponente
+	 * @return Resultado de la operacionn base elevado a exponente
 	 */
 	public double exponent(float base, float exponent) {
 		double result =  Math.pow(base, exponent);
 		return result;
-	} // Cierre del método
+	} // Cierre del metodo
 	
 } // Cierre de la clase
 
