5d732263551c9631a9dafc85b35cf7f4d8055a53
ThattaPy <56270213+ThattaPY@users.noreply.github.com>
2021-02-27 00:04:44 +0100

    Creados dos test de la clase Suma

--- Calculadora_EntornosDesarrollo/src/testOperaciones/TestSuma.java
+++ Calculadora_EntornosDesarrollo/src/testOperaciones/TestSuma.java
@@ -10,12 +10,22 @@ class TestSuma {
 
 	@Test
 	void esSumaValoresReales() {
-		fail("Not yet implemented");
+		Suma sum = new Suma();
+		double num1 = 2.3;
+		double num2 = 7.7;
+		double esperado = 10;
+		assertEquals(esperado, sum.esSumaValoresReales(num1, num2));
+		
 	}
 
 	@Test
 	void esSumaValoresEnteros() {
-		fail("Not yet implemented");
+		Suma sum = new Suma();
+		int num1 = 8;
+		int num2 = 9;
+		double esperado = 17;
+		assertEquals(esperado, sum.esSumaValoresReales(num1, num2));	
+	
 	}
 
 	@Test
