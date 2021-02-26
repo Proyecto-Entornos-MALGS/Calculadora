a7321754c6311c1f99151e306c98e50c17e528a2
ThattaPy <56270213+ThattaPY@users.noreply.github.com>
2021-02-26 23:59:41 +0100

    Creada la clase TestSuma

---
+++ Calculadora_EntornosDesarrollo/src/testOperaciones/TestSuma.java
@@ -0,0 +1,35 @@
+package testOperaciones;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import operaciones.Suma;
+
+class TestSuma {
+
+	@Test
+	void esSumaValoresReales() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void esSumaValoresEnteros() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void esSumaMultiple() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void esSumaAcumulador() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void getAcumulador() {
+		fail("Not yet implemented");
+	}
+}
