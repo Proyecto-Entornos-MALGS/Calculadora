6b3245d71151716f25756b4ac85ae0d207d1b6cc
ThattaPy <56270213+ThattaPY@users.noreply.github.com>
2021-02-27 12:15:11 +0100

    Creada clase testResta

---
+++ Calculadora_EntornosDesarrollo/src/testOperaciones/TestResta.java
@@ -0,0 +1,36 @@
+package testOperaciones;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import operaciones.Suma;
+
+class TestResta {
+
+	@Test
+	void testSumaValoresReales() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSumaValoresEnteros() {
+		fail("Not yet implemented");	
+	
+	}
+
+	@Test
+	void testSumaMultiple() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSumaAcumulador() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void getAcumulador() {
+		fail("Not yet implemented");
+	}
+}
