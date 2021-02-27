0c7fc656de56341622424b6d13627a0bab2e70c8
ThattaPy <56270213+ThattaPY@users.noreply.github.com>
2021-02-27 12:18:55 +0100

    Creado metodo testResta2Reales

--- Calculadora_EntornosDesarrollo/src/testOperaciones/TestResta.java
+++ Calculadora_EntornosDesarrollo/src/testOperaciones/TestResta.java
@@ -4,13 +4,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-import operaciones.Suma;
+import operaciones.Resta;
 
 class TestResta {
 
 	@Test
-	void testSumaValoresReales() {
-		fail("Not yet implemented");
+	void testResta2Reales() {
+		Resta rest = new Resta();
+		double num1 = 3.3;
+		double num2 = 2.1;
+		double esperado = 1.2;
+		assertEquals(esperado, rest.resta2Reales(num1, num2));
 	}
 
 	@Test
