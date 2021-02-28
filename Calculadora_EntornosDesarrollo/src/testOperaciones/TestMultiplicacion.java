7e12670de33440010ceba8d8a40e590fa8706421
miguel perez larren <miguelperezlarren@hotmail.com>
2021-02-28 18:49:35 +0100

    Insetado test de multiplicaciones

---
+++ Calculadora_EntornosDesarrollo/src/testOperaciones/TestMultiplicacion.java
@@ -0,0 +1,14 @@
+package testOperaciones;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestMultiplicacion {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
