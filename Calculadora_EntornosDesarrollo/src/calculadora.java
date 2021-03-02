d6ff3668f499091190e213c469dad66f4c66dc2a
ThattaPy <56270213+ThattaPY@users.noreply.github.com>
2021-03-02 16:12:04 +0100

    Creadas pruebas en clase main

--- Calculadora_EntornosDesarrollo/src/calculadora.java
+++ Calculadora_EntornosDesarrollo/src/calculadora.java
@@ -17,10 +17,11 @@ public class calculadora {
 		
 		System.out.println(division.dividirReales(16.3, 2.65));
 		
-		
-		
-		
-		
+		System.out.println(multi.twoRealNumbersProduct(16.2, 2.5));
+		System.out.println(multi.twoIntegerNumersProduct(16, 2));
+		System.out.println(multi.threeRealNumbersProduct(16.2F, 2.6F, 2.1F));
+		System.out.println(multi.exponent(16, 2));
+
 		
 		
 		
